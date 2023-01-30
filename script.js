11488a8158c91425c922f5b36075e676ccc2bf0b
DewaSRY <sdewa6645@gmail.com>
2023-01-30 17:41:09 +0800

    done

--- script.js
+++ script.js
@@ -1,81 +1,40 @@
-const toggleswitch = document.querySelector('input[type="checkbox"]');
-const nav = document.getElementById("nav");
-const toggleIcon = document.getElementById("toggle-icon");
-const image1 = document.getElementById("image1");
-const image2 = document.getElementById("image2");
-const image3 = document.getElementById("image3");
-const textBox = document.getElementById("text-box");
+const menuBars = document.getElementById("menu-bars");
+const overlay = document.getElementById("overlay");
+const nav1 = document.getElementById("nav-1");
+const nav2 = document.getElementById("nav-2");
+const nav3 = document.getElementById("nav-3");
+const nav4 = document.getElementById("nav-4");
+const nav5 = document.getElementById("nav-5");
 //
-
-// Dark or Light Images
-function imageMode(color) {
-  image1.src = `img/undraw_proud_coder_${color}.svg`;
-  image2.src = `img/undraw_feeling_proud_${color}.svg`;
-  image3.src = `img/undraw_conceptual_idea_${color}.svg`;
-}
-// Dark Mode Styles
-// function darkMode() {
-//   document.documentElement.setAttribute("data-theme", "dark");
-//   nav.style.backgroundColor = "rgb(0 0 0 / 50%)";
-//   textBox.style.backgroundColor = "rgb(255 255 255 / 50%)";
-//   toggleIcon.children[0].textContent = "Dark Mode";
-//   toggleIcon.children[1].classList.replace("fa-sun", "fa-moon");
-//   imageMode("dark");
-//   localStorage.setItem("theme", "dark");
-// }
-// Light Mode Styles
-// function lightMode() {
-//   document.documentElement.setAttribute("data-theme", "light");
-//   nav.style.backgroundColor = "rgb(255 255 255 / 50%)";
-//   textBox.style.backgroundColor = "rgb(0 0 0 / 50%)";
-//   toggleIcon.children[0].textContent = "Light Mode";
-//   toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
-//   imageMode("light");
-//   localStorage.setItem("theme", "light");
-// }
-function toggleMode(theme) {
-  const temaMode = `${theme ? "dark" : "light"}`;
-  document.documentElement.setAttribute("data-theme", temaMode);
-  nav.style.backgroundColor = theme
-    ? "rgb(0 0 0 / 50%)"
-    : "rgb(255 255 255 / 50%)";
-  textBox.style.backgroundColor = theme
-    ? "rgb(255 255 255 / 50%)"
-    : "rgb(0 0 0 / 50%)";
-  toggleIcon.children[0].textContent = theme ? "Dark Mode" : "Light Mode";
-  theme
-    ? toggleIcon.children[1].classList.replace("fa-sun", "fa-moon")
-    : toggleIcon.children[1].classList.replace("fa-moon", "fa-sun");
-  imageMode(temaMode);
-  localStorage.setItem("choice", theme);
-  //   localStorage.setItem("theme", temaMode);
-}
-// togle methode
-let currentChoiceTheme = localStorage.getItem("choice");
-toggleMode(currentChoiceTheme);
-currentChoiceTheme
-  ? (toggleswitch.checked = true)
-  : (toggleswitch.checked = false);
-
-function swichTheme(event) {
-  const checkStatus = event.target.checked;
-  toggleMode(checkStatus);
-  currentChoiceTheme = localStorage.getItem("choice");
-  //   console.log(checkStatus);
-  //   checkStatus ? darkMode() : lightMode();
-  //   console.log(currentChoiceTheme);
+const navItems = [nav1, nav2, nav3, nav4, nav5];
+//
+function navAnimation(direction1, direction2) {
+  navItems.forEach((nav, i) => {
+    nav.classList.replace(
+      `slide-${direction1}-${i + 1}`,
+      `slide-${direction2}-${i + 1}`
+    );
+  });
 }
 
-//  Event Listener
-toggleswitch.addEventListener("change", swichTheme);
-
-// Check Local Storage For Theme
-// const currentTheme = localStorage.getItem("theme");
-// if (currentTheme) {
-//   document.documentElement.setAttribute("data-theme", currentTheme);
-//   if (currentTheme === "dark") {
-//     toggleswitch.checked = true;
-//     toggleMode(true);
-//     // darkMode();
-//   }
-// }
+function toggleNav() {
+  // Toggle: Menu Bars Open/Closed
+  menuBars.classList.toggle("change");
+  // Toggle: Menu Active
+  overlay.classList.toggle("overlay-active");
+  if (overlay.classList.contains("overlay-active")) {
+    overlay.classList.replace("overlay-slide-left", "overlay-slide-right");
+    navAnimation("out", "in");
+  } else {
+    overlay.classList.replace("overlay-slide-right", "overlay-slide-left");
+    navAnimation("in", "out");
+  }
+}
+menuBars.addEventListener("click", toggleNav);
+navItems.forEach((nav) => {
+  nav.addEventListener("click", toggleNav);
+});
+// navItems.forEach((nav, id) => {
+//   console.log(nav);
+//   console.log(id);
+// });
