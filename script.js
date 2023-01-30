ff3b13fbd9e13d3a263d3bbfc7a0762d42491f5b
DewaSRY <sdewa6645@gmail.com>
2023-01-30 11:26:22 +0800

    done

--- script.js
+++ script.js
@@ -1,91 +1,147 @@
-const imageContainer = document.getElementById("image-container");
-const loader = document.getElementById("loader");
+//alement
+const button = document.getElementById("button");
+const audioElement = document.getElementById("audio");
+// VoiceRSS Javascript SDK
+const VoiceRSS = {
+  speech: function (e) {
+    this._validate(e), this._request(e);
+  },
+  _validate: function (e) {
+    if (!e) throw "The settings are undefined";
+    if (!e.key) throw "The API key is undefined";
+    if (!e.src) throw "The text is undefined";
+    if (!e.hl) throw "The language is undefined";
+    if (e.c && "auto" != e.c.toLowerCase()) {
+      var a = !1;
+      switch (e.c.toLowerCase()) {
+        case "mp3":
+          a = new Audio().canPlayType("audio/mpeg").replace("no", "");
+          break;
+        case "wav":
+          a = new Audio().canPlayType("audio/wav").replace("no", "");
+          break;
+        case "aac":
+          a = new Audio().canPlayType("audio/aac").replace("no", "");
+          break;
+        case "ogg":
+          a = new Audio().canPlayType("audio/ogg").replace("no", "");
+          break;
+        case "caf":
+          a = new Audio().canPlayType("audio/x-caf").replace("no", "");
+      }
+      if (!a) throw "The browser does not support the audio codec " + e.c;
+    }
+  },
+  _request: function (e) {
+    var a = this._buildRequest(e),
+      t = this._getXHR();
+    (t.onreadystatechange = function () {
+      if (4 == t.readyState && 200 == t.status) {
+        if (0 == t.responseText.indexOf("ERROR")) throw t.responseText;
+
+        (audioElement.src = t.responseText), audioElement.play();
+      }
+    }),
+      t.open("POST", "https://api.voicerss.org/", !0),
+      t.setRequestHeader(
+        "Content-Type",
+        "application/x-www-form-urlencoded; charset=UTF-8"
+      ),
+      t.send(a);
+  },
+  _buildRequest: function (e) {
+    var a = e.c && "auto" != e.c.toLowerCase() ? e.c : this._detectCodec();
+    return (
+      "key=" +
+      (e.key || "") +
+      "&src=" +
+      (e.src || "") +
+      "&hl=" +
+      (e.hl || "") +
+      "&r=" +
+      (e.r || "") +
+      "&c=" +
+      (a || "") +
+      "&f=" +
+      (e.f || "") +
+      "&ssml=" +
+      (e.ssml || "") +
+      "&b64=true"
+    );
+  },
+  _detectCodec: function () {
+    var e = new Audio();
+    return e.canPlayType("audio/mpeg").replace("no", "")
+      ? "mp3"
+      : e.canPlayType("audio/wav").replace("no", "")
+      ? "wav"
+      : e.canPlayType("audio/aac").replace("no", "")
+      ? "aac"
+      : e.canPlayType("audio/ogg").replace("no", "")
+      ? "ogg"
+      : e.canPlayType("audio/x-caf").replace("no", "")
+      ? "caf"
+      : "";
+  },
+  _getXHR: function () {
+    try {
+      return new XMLHttpRequest();
+    } catch (e) {}
+    try {
+      return new ActiveXObject("Msxml3.XMLHTTP");
+    } catch (e) {}
+    try {
+      return new ActiveXObject("Msxml2.XMLHTTP.6.0");
+    } catch (e) {}
+    try {
+      return new ActiveXObject("Msxml2.XMLHTTP.3.0");
+    } catch (e) {}
+    try {
+      return new ActiveXObject("Msxml2.XMLHTTP");
+    } catch (e) {}
+    try {
+      return new ActiveXObject("Microsoft.XMLHTTP");
+    } catch (e) {}
+    throw "The browser does not support HTTP request";
+  },
+};
+// api control
+let joke = "";
 //
-let ready = false;
-let imagesLoaded = 0;
-let totalImage = 0;
-let photoArray = [];
-//Api
-const YOUR_ACCESS_KEY = `v2SqFi-a1M6rNzU7314v-mO-yl91Py36MktXT2iVeqY`;
-const Secret_key = `FIh3eCmF0wAagk1tjJi8lGEnp5Bb-nLjp92b3QcrOPQ`;
-let count = 5;
-const apiUrl = `https://api.unsplash.com/photos/random/?client_id=${YOUR_ACCESS_KEY}&count=${count}`;
-// Helper Function to set Atribut
-function setAttributs(element, attributes) {
-  for (const key in attributes) {
-    element.setAttribute(key, attributes[key]);
+const jokeUrl = "https://v2.jokeapi.dev/joke/Programming";
+const ApiKey = "c7fa41b4603d4c958a0c5bd33c503a90";
+// getJokes
+async function getJokes() {
+  try {
+    const response = await fetch(jokeUrl);
+    const data = await response.json();
+    data.setup ? (joke = `${data.setup},${data.delivery}`) : (joke = data.joke);
+    tellMe(joke);
+  } catch (error) {
+    console.log("something was wrong", error);
   }
 }
-
-// display phot
-function displayPhoto() {
-  photoArray.forEach((photo) => {
-    imagesLoaded = 0;
-    totalImage = photoArray.length;
-    // console.log("total image", totalImage);
-    // crete a link to uspas
-    const item = document.createElement("a");
-    /*
-    item.setAttribute("herf", photo.links.html);
-    item.setAttribute("target", "_blank");
-    */
-    setAttributs(item, {
-      herf: photo.links.html,
-      target: "_blank",
-    });
-    // Create emage for photo
-    const img = document.createElement("img");
-    /*
-    img.setAttribute("src", photo.urls.regular);
-    img.setAttribute("alt", photo.alt_description);
-    img.setAttribute("title", photo.alt_description);
-    */
-
-    setAttributs(img, {
-      src: photo.urls.regular,
-      alt: photo.alt_description,
-      title: photo.alt_description,
-    });
-    // Evwnt listener
-    img.addEventListener("load", imageLoaded);
-    // put img in itme and add it to container
-    item.appendChild(img);
-    imageContainer.appendChild(item);
+// telmee
+function tellMe(a_joke) {
+  const my_joke = `i have a joke, ${a_joke}`;
+  toggleButton();
+  console.log(my_joke);
+  VoiceRSS.speech({
+    key: ApiKey,
+    src: my_joke,
+    hl: "en-us",
+    v: "Linda",
+    r: 0,
+    c: "mp3",
+    f: "44khz_16bit_stereo",
+    ssml: false,
   });
 }
-// imageLoaded
-function imageLoaded() {
-  imagesLoaded++;
-  //   console.log("image loaded", imagesLoaded);
-  if (imagesLoaded === totalImage) {
-    ready = true;
-    loader.hidden = true;
-    count = 10;
-    // console.log("ready", ready);
-  }
+// button togle
+function toggleButton() {
+  button.disabled = !button.disabled;
 }
-
-// Get photo
-async function getPhoto() {
-  try {
-    const response = await fetch(apiUrl);
-    photoArray = await response.json();
-    displayPhoto();
-    // console.log(photoArray);
-  } catch (error) {
-    // console.log("sometinge was wrong", error);
-  }
-}
-// Check see if scroling near bottom of page
-window.addEventListener("scroll", () => {
-  if (
-    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
-    ready
-  ) {
-    console.log("scroled");
-    getPhoto();
-    ready = false;
-  }
-});
 // load
-getPhoto();
+// event
+button.addEventListener("click", getJokes);
+audioElement.addEventListener("ended", toggleButton);
