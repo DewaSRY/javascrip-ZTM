6a1ab75fb4df798dbb7050dd1c87682d0d728a57
DewaSRY <sdewa6645@gmail.com>
2023-05-16 16:59:56 +0800

    add event listener

--- index.js
+++ index.js
@@ -1,55 +1,70 @@
-const quoteContainer = document.getElementById("quote-container");
-const quoteText = document.getElementById("quote");
-const authorText = document.getElementById("author");
-const twitterBtn = document.getElementById("twitter");
-const newQuoteBtn = document.getElementById("new-quote");
-const loaderAnimation = document.getElementById("loader");
-// Get Quotes from api
-let apiQuotes = [];
-// newQuote
-function newQuote() {
-  // pick a random quote from apiQuotes array
-  const randomIndex = Math.floor(Math.random() * apiQuotes.length);
-  const quote = apiQuotes[randomIndex];
-  authorText.textContent = quote.author === null ? quote.tag : quote.author;
-  quoteText.textContent = quote.text;
-  quote.text.length > 35
-    ? quoteText.classList.add("long-quote")
-    : quoteText.classList.remove("long-quote");
-  complite();
+const imageContainer = document.getElementById("image-container");
+const loadder = document.getElementById("loadder");
+const Client_id = "Jw3G2Vx4vAuKLaMKXvx9E3AmHP6qU3qCr8RjzWGHuOw";
+const count = 10;
+const apiUrl = `https://api.unsplash.com/photos/random?client_id=${Client_id}&count=${count}`;
+//Unsplash Api
+let ready = false;
+let imagesHaveLoadded = 0;
+let totalImages = 0;
+let photoArray = [];
+//Create Elements for links & Photos add to DOM
+function setAttributs(Elements, attributs) {
+  for (const key in attributs) [Elements.setAttribute(key, attributs[key])];
 }
-// get getQuotes
-async function getQuotes() {
-  loading();
-  const apiUrl = "https://jacintodesign.github.io/quotes-api/data/quotes.json";
-  try {
-    const response = await fetch(apiUrl);
-    apiQuotes = await response.json();
-    newQuote();
-  } catch (error) {
-    //
-    getQuotes();
-    console.log("somthing wrong", error);
+//Check if all images were loadded
+function imagesLoadded() {
+  console.log("image", imagesHaveLoadded);
+  imagesHaveLoadded++;
+  if (imagesHaveLoadded === totalImages) {
+    loadder.classList.add("hidden");
+    ready = true;
+    console.log("readey=", ready);
   }
 }
-// Tweet Quote
-function tweetQuote() {
-  //
-  const tweetApiUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
-  window.open(tweetApiUrl, "_blank");
+function displayPhoto() {
+  imagesHaveLoadded = 0;
+  totalImages = photoArray.length;
+  photoArray.forEach((photo) => {
+    //Create <a> to Link to Unsplash
+    const item = document.createElement("a");
+    const img = document.createElement("img");
+    setAttributs(item, {
+      href: photo.links.html,
+      target: "_blank",
+    });
+    setAttributs(img, {
+      src: photo.urls.regular,
+      alt: photo.alt_description,
+      title: photo.alt_description,
+    });
+    img.addEventListener("load", imagesLoadded);
+    //Put <img> inside <a>, then put both inside Image Container
+    item.appendChild(img);
+    imageContainer.appendChild(item);
+  });
 }
-// loading animation
-function complite() {
-  loaderAnimation.hidden = true;
-  quoteContainer.hidden = false;
+//get Photos from Unsplas api
+async function getPhotos() {
+  try {
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    photoArray.push(...data);
+    displayPhoto();
+  } catch (err) {
+    console.log(err);
+  }
 }
-function loading() {
-  loaderAnimation.hidden = false;
-  quoteContainer.hidden = true;
-}
-
-// on load
-getQuotes();
-// listen
-twitterBtn.addEventListener("click", tweetQuote);
-newQuoteBtn.addEventListener("click", getQuotes);
+//Check to see if scrolling near bottom of page, load more photos
+window.addEventListener("scroll", () => {
+  if (
+    window.innerHeight + window.scrollY >= document.body.offsetHeight - 1000 &&
+    ready
+  ) {
+    loadder.classList.remove("hidden");
+    ready = false;
+    getPhotos();
+  }
+});
+//On Load
+getPhotos();
